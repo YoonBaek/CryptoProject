95233f4a035935e6215386227f1930c3fe877bf9
YoonBaek <royal729@naver.com>
2021-12-10 18:43:17 +0900

    code: Add cli/mode "greedy"

--- cli/cli.go
+++ cli/cli.go
@@ -12,7 +12,7 @@ import (
 func usage() {
 	fmt.Printf("Welcome to Crypto Project\n\n")
 	fmt.Printf("Pealse use the following commands:\n\n")
-	fmt.Printf("-mode:	Choose between 'rest' and 'explorer'\n")
+	fmt.Printf("-mode:	Choose between 'rest' and 'explorer'\n	If you want both, 'greedy' is ready for you\n")
 	fmt.Printf("-port:	Sets the port number of the server\n\n")
 	os.Exit(0)
 }
@@ -21,7 +21,7 @@ func Start() {
 	if len(os.Args) == 1 {
 		usage()
 	}
-	mode := flag.String("mode", "rest", "Choose between 'rest' and 'explorer'")
+	mode := flag.String("mode", "rest", "Choose between 'rest' and 'explorer'\nIf you want both, 'greedy' is ready for you\n")
 	port := flag.Int("port", 4000, "Sets the port number of the server")
 	flag.Parse()
 
@@ -32,6 +32,9 @@ func Start() {
 	case "explorer":
 		// start html explorer
 		explorer.Start(*port)
+	case "greedy":
+		go rest.Start(*port)
+		explorer.Start(*port + 1)
 	default:
 		usage()
 	}
