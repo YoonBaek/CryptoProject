eacb8854efdf1b19f0ccefc76371510ec44e8aec
YoonBaek <royal729@naver.com>
2021-12-10 20:10:25 +0900

    code: Add db pkg

---
+++ blockchain/db/db.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"github.com/YoonBaek/CryptoProject/utils"
+	"github.com/boltdb/bolt"
+)
+
+const (
+	dbName      = "blockchain.db"
+	dataBucket  = "data"
+	blockBucket = "blocks"
+)
+
+var db *bolt.DB
+
+// This function is sigleton pattern of loading blockchain db
+// if block chain db has not been loaded to memory,
+// load db to memory, and construct buckets,
+func DB() *bolt.DB {
+	if db == nil {
+		dbPointer, err := bolt.Open(dbName, 0600, nil)
+		utils.HandleErr(err)
+		db = dbPointer
+
+		err = db.Update(func(t *bolt.Tx) error {
+			_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
+			utils.HandleErr(err)
+			_, err = t.CreateBucketIfNotExists([]byte(blockBucket))
+			return err
+		})
+		utils.HandleErr(err)
+	}
+	return db
+}
