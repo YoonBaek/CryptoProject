bce576877b0e2e7489d00f816c47a9ce0bbe311b
YoonBaek <royal729@naver.com>
2021-12-16 10:58:52 +0900

    feat: Add timestamp for block creation automated

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -7,9 +7,17 @@ import (
 	"github.com/YoonBaek/CryptoProject/utils"
 )
 
+const (
+	defaultDifficulty  int = 2
+	difficultyInterval int = 7
+	blockInterval      int = 2
+	bound              int = 3
+)
+
 type blockchain struct {
-	LatestHash string `json:"latestHash"`
-	Height     int    `json:"height"`
+	LatestHash        string `json:"latestHash"`
+	Height            int    `json:"height"`
+	CurrentDifficulty int    `json:"currentdifficulty"`
 }
 
 var (
@@ -25,10 +33,37 @@ func (b *blockchain) persist() {
 	db.SaveCheckpoint(utils.ToBytes(b))
 }
 
+func (b *blockchain) recalculateDifficulty() int {
+	allBlocks := b.Blocks()
+	newestBlock := allBlocks[0]
+	lastRecalculatedBlock := allBlocks[difficultyInterval-1]
+	actualTime := (newestBlock.Timestamp - lastRecalculatedBlock.Timestamp) / 60
+	expectedTime := difficultyInterval * blockInterval
+	// fmt.Println("-------", actualTime, expectedTime-bound)
+	if actualTime < expectedTime-bound {
+		return b.CurrentDifficulty + 1
+	}
+	if actualTime > expectedTime+bound {
+		return b.CurrentDifficulty
+	}
+	return b.CurrentDifficulty
+}
+
+func (b *blockchain) difficulty() int {
+	if b.Height == 0 {
+		return defaultDifficulty
+	}
+	if b.Height%difficultyInterval == 0 {
+		return b.recalculateDifficulty()
+	}
+	return b.CurrentDifficulty
+}
+
 func (b *blockchain) AddBlock(data string) {
 	block := createBlock(data, b.LatestHash, b.Height+1)
 	b.LatestHash = block.Hash
 	b.Height = block.Height
+	b.CurrentDifficulty = block.Difficulty
 	b.persist()
 }
 
@@ -38,9 +73,9 @@ func BlockChain() *blockchain {
 		// 그리고 수 많은 go routine 들이 GetBlockChain을 호출한다고 할 때,
 		// b 인스턴스는 한 번만 제대로 초기화 되면 된다.
 		// 그럴 때 동기 처리를 도와주는 sync 패키지를 활용한다.
-		// once.Do()는 메서드 인자에 들어온 함수를 딱 한 번만 실행하는 기능이다.
+		// once.Do()는 메서드 인자에 들어온 함수를 딱 한 번만 실행하는 기능이다.e.Do()는 메서드 인자에 들어온 함수를 딱 한 번만 실행하는 기능이다.
 		once.Do(func() {
-			b = &blockchain{"", 0}
+			b = &blockchain{Height: 0}
 			checkpoint := db.LoadCheckpoint()
 			if checkpoint == nil {
 				b.AddBlock("Genesis Block")
