1fe7aae9a272bf84f74440685d6e22acf70bf7cf
YoonBaek <royal729@naver.com>
2021-12-16 15:00:32 +0900

    code: Add TxOuts, TxOutsByAddr, BalanceByAddr

--- blockchain/transactions.go
+++ blockchain/transactions.go
@@ -21,13 +21,13 @@ func (t *Tx) getId() {
 }
 
 type TxIn struct {
-	Owner  string
-	Amount int
+	Owner  string `json:"owner"`
+	Amount int    `json:"amount"`
 }
 
 type TxOut struct {
-	Owner  string
-	Amount int
+	Owner  string `json:"owner"`
+	Amount int    `json:"amount"`
 }
 
 func makeCoinbaseTx(address string) *Tx {
@@ -45,4 +45,36 @@ func makeCoinbaseTx(address string) *Tx {
 	}
 	tx.getId()
 	return &tx
+}
+
+// TxOut들을 모아놓은 슬라이스 만들기
+func (b *blockchain) txOuts() (txOuts []*TxOut) {
+	for _, block := range b.Blocks() {
+		for _, tx := range block.TransActions {
+			txOuts = append(txOuts, tx.TxOuts...)
+		}
+	}
+	return
+}
+
+// TxOut 슬라이스 중 address에 맞는 것 걸러주기 -> export
+func (b *blockchain) TxOutsByAddr(addr string) (txOutsByAddr []*TxOut) {
+	txOuts := b.txOuts()
+
+	for _, txOut := range txOuts {
+		if txOut.Owner != addr {
+			continue
+		}
+		txOutsByAddr = append(txOutsByAddr, txOut)
+	}
+	return
+}
+
+func (b *blockchain) BalanceByAddr(addr string) (total int) {
+	txOuts := b.TxOutsByAddr(addr)
+
+	for _, txOut := range txOuts {
+		total += txOut.Amount
+	}
+	return
 }
