1a8b2daf02d7f927cae9524d2eb51fea0e385da6
YoonBaek <royal729@naver.com>
2021-12-29 12:06:02 +0900

    feat: Add Signature to msg

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -4,6 +4,7 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"encoding/hex"
 	"fmt"
 
 	"github.com/YoonBaek/CryptoProject/utils"
@@ -12,5 +13,14 @@ import (
 func Start() {
 	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	utils.HandleErr(err)
-	fmt.Println(privateKey.D)
+	targetMessage := "you are amazing"
+	hashedMessage := utils.Hash(targetMessage)
+
+	hash, err := hex.DecodeString(hashedMessage)
+	utils.HandleErr(err)
+	r, s, err := ecdsa.Sign(rand.Reader, privateKey, hash)
+	utils.HandleErr(err)
+
+	fmt.Println("R:", r)
+	fmt.Println("S:", s)
 }
