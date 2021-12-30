8cd1897339e4262d3cbef9c6f31529a8db057838
YoonBaek <royal729@naver.com>
2021-12-30 11:22:32 +0900

    feat: Add wallet restore and address + implement to Tx

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -5,6 +5,7 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/x509"
+	"fmt"
 	"os"
 
 	"github.com/YoonBaek/CryptoProject/utils"
@@ -17,14 +18,28 @@ const (
 // follows singleton pattern
 type wallet struct {
 	privateKey *ecdsa.PrivateKey
+	Address    string
 }
 
 var w *wallet
 
-func (w *wallet) createPrivKey() {
+func (w *wallet) createKey() {
 	privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	utils.HandleErr(err)
 	w.privateKey = privKey
+}
+
+func (w *wallet) restoreKey() {
+	keyAsBytes, err := os.ReadFile(fileName)
+	utils.HandleErr(err)
+	restoredKey, err := x509.ParseECPrivateKey(keyAsBytes)
+	utils.HandleErr(err)
+	w.privateKey = restoredKey
+}
+
+func (w *wallet) getAddress(pubKey *ecdsa.PublicKey) {
+	address := append(pubKey.X.Bytes(), pubKey.Y.Bytes()...)
+	w.Address = fmt.Sprintf("%x", address)
 }
 
 func Wallet() *wallet {
@@ -32,13 +47,14 @@ func Wallet() *wallet {
 	if w == nil {
 		w = &wallet{}
 		// 해당 지갑 정보가 있는지 확인하기
-		if hasWallet() {
-			// 있다면 파일로부터 불러오기
-			return w
+		switch hasWallet() {
+		case true: // 있다면 파일로부터 불러오기
+			w.restoreKey()
+		default: // 없다면 비공개키를 생성해 주고 파일에 저장
+			w.createKey()
+			persistKey(w.privateKey)
 		}
-		// 없다면 비공개키를 생성해 주고 파일에 저장
-		w.createPrivKey()
-		persistKey(w.privateKey)
+		w.getAddress(&w.privateKey.PublicKey)
 	}
 	return w
 }
